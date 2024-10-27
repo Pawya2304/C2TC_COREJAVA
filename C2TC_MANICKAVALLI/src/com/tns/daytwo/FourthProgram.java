42c91e86d1fbbcc6ed3f2caf3613a845de160dfa
HP <HP@LAPTOP-0RPLUSCA>
2024-10-27 21:29:13 +0530

    i need to commmit

---
+++ C2TC_MANICKAVALLI/src/com/tns/daytwo/FourthProgram.java
@@ -0,0 +1,10 @@
+package com.tns.daytwo;
+
+public class FourthProgram {
+	public static void main(String[] args) {
+		int x = 5;
+		if (x != 5)
+		System.out.println("Value of x is not 5");
+		}
+		}
+
